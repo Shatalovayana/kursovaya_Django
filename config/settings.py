82ce9b3bc37360b1987dbb806bd6aea1efbc6639
Yana <yanashat198@gmail.com>
2023-12-13 12:06:59 +0700

    add settings.py

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mailing',
+        'USER': 'postgres',
+        'PASSWORD': '373479060'
     }
 }
 
